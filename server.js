73362e571b4a7886e4a8aad9e3d046f297f66e19
Elistar_DJ <elistardj@outlook.com>
2026-03-25 15:04:15 +0300

    add a few loggers, connect its to users routes

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const User = require("./user_schema");
-require('dotenv').config()
+const {
+  commonLogger,
+  withIdMiddleware,
+  withoutIdMiddleware,
+} = require("./middleware");
+require("dotenv").config();
 
 const PORT = process.env.DEV_PORT;
 const USERS_PATH = "/api/users";
@@ -10,6 +15,7 @@ const USERS_PATH_WITH_ID = USERS_PATH + "/:id";
 const users = [];
 
 app.use(express.json());
+app.use(commonLogger);
 
 app.get("/api/hello", (req, res) => {
   res.send("Привет, Redev!");
@@ -20,60 +26,62 @@ app.post("/api/echo", (req, res) => {
   res.send(JSON.stringify(message));
 });
 
-app.get(USERS_PATH, (req, res) => {
-  res.send(JSON.stringify(users));
-});
+app
+  .route(USERS_PATH)
+  .all(withoutIdMiddleware)
+  .get((req, res) => {
+    res.send(JSON.stringify(users));
+  })
+  .post((req, res) => {
+    const newUser = new User(req.body);
+    users.push(newUser);
+    res.send(JSON.stringify(newUser));
+  });
 
-app.get(USERS_PATH_WITH_ID, (req, res) => {
-  const { id } = req.params;
-  const user = users.find((user) => user.id === id);
-  if (!user) {
-    res.send(JSON.stringify("User not found."));
-  }
-  res.send(JSON.stringify(user));
-});
-
-app.post(USERS_PATH, (req, res) => {
-  const newUser = new User(req.body);
-  users.push(newUser);
-  res.send(JSON.stringify(newUser));
-});
-
-app.put(USERS_PATH_WITH_ID, (req, res) => {
-  const data = req.body;
-  const { id } = req.params;
-  const targetUserIndex = users.findIndex((user) => user.id === id);
-  if (targetUserIndex === -1) {
-    res.send(JSON.stringify("User not found."));
-  }
-  const updatedUser = new User({ id, ...data });
-  users[targetUserIndex] = updatedUser;
-  res.send(JSON.stringify(updatedUser));
-});
-
-app.patch(USERS_PATH_WITH_ID, (req, res) => {
-  const { id } = req.params;
-  const { password } = req.body;
-  const targetUserIndex = users.findIndex((user) => user.id === id);
-  if (targetUserIndex === -1) {
-    res.send(JSON.stringify("User not found."));
-  }
-  if (!password) {
-    res.send(JSON.stringify("Password is required."));
-  }
-  users[targetUserIndex].password = password;
-  res.send(JSON.stringify(id));
-});
-
-app.delete(USERS_PATH_WITH_ID, (req, res) => {
-  const { id } = req.params;
-  const targetUserIndex = users.findIndex((user) => user.id === id);
-  if (targetUserIndex === -1) {
-    res.send(JSON.stringify("User not found."));
-  }
-  users.splice(targetUserIndex, 1);
-  res.send(JSON.stringify(id));
-});
+app
+  .route(USERS_PATH_WITH_ID)
+  .all(withIdMiddleware)
+  .get((req, res) => {
+    const { id } = req.params;
+    const user = users.find((user) => user.id === id);
+    if (!user) {
+      return res.send(JSON.stringify("User not found."));
+    }
+    res.send(JSON.stringify(user));
+  })
+  .put((req, res) => {
+    const data = req.body;
+    const { id } = req.params;
+    const targetUserIndex = users.findIndex((user) => user.id === id);
+    if (targetUserIndex === -1) {
+      return res.send(JSON.stringify("User not found."));
+    }
+    const updatedUser = new User({ id, ...data });
+    users[targetUserIndex] = updatedUser;
+    res.send(JSON.stringify(updatedUser));
+  })
+  .patch((req, res) => {
+    const { id } = req.params;
+    const { password } = req.body;
+    const targetUserIndex = users.findIndex((user) => user.id === id);
+    if (targetUserIndex === -1) {
+      return res.send(JSON.stringify("User not found."));
+    }
+    if (!password) {
+      return res.send(JSON.stringify("Password is required."));
+    }
+    users[targetUserIndex].password = password;
+    res.send(JSON.stringify(id));
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const targetUserIndex = users.findIndex((user) => user.id === id);
+    if (targetUserIndex === -1) {
+      return res.send(JSON.stringify("User not found."));
+    }
+    users.splice(targetUserIndex, 1);
+    res.send(JSON.stringify(id));
+  });
 
 app.listen(PORT, () => {
   console.log(`Started on http://localhost:${PORT}`);
